df05b4d57e05651ff75e6231c2dc56765f56e5e0
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-06-06 14:47:48 -0600

    chore: update flutter course

--- widgets_app/lib/config/router/router.dart
+++ widgets_app/lib/config/router/router.dart
@@ -2,7 +2,14 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-  GoRoute(path: '/buttons', builder: (context, state) => const ButtonsScreen()),
-  GoRoute(path: '/cards', builder: (context, state) => const CardScreen())
+  GoRoute(
+      name: 'home', path: '/', builder: (context, state) => const HomeScreen()),
+  GoRoute(
+      name: 'buttons',
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen()),
+  GoRoute(
+      name: 'cards',
+      path: '/cards',
+      builder: (context, state) => const CardScreen())
 ]);
