7a2b90da0d3d609454897b937855c488e3e81b7a
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-06-06 15:25:40 -0600

    chore: wip buttons container

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -79,8 +79,35 @@ class _ButtonsView extends StatelessWidget {
               style: ButtonStyle(
                   backgroundColor: WidgetStatePropertyAll(colors.primary),
                   iconColor: WidgetStatePropertyAll(colors.onPrimary)),
-            )
+            ),
+            const _CustomButton()
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomButton extends StatelessWidget {
+  const _CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Custom Button',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
