9e8612b9b13821576adccbbccd680257239b53b9
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-06-07 09:51:47 -0600

    chore: wip screens for next lection

---
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorial extends StatelessWidget {
+  static String name = 'apptutorial_screen';
+
+  const AppTutorial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
