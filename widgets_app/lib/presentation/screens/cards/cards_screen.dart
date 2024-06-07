4d2be68efdc499e175788c875212a45b9285b209
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-06-06 20:06:38 -0600

    chore: wip flutter course

--- widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {
+    'elevation': 0.0,
+    'label': 'Elevation 0',
+  },
+  {
+    'elevation': 1.0,
+    'label': 'Elevation 1',
+  },
+  {
+    'elevation': 2.0,
+    'label': 'Elevation 2',
+  },
+  {
+    'elevation': 3.0,
+    'label': 'Elevation 3',
+  },
+  {
+    'elevation': 4.0,
+    'label': 'Elevation 4',
+  },
+];
+
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
 
@@ -9,8 +32,56 @@ class CardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Card Screen'),
       ),
-      body: const Center(
-        child: Text('Card Screen'),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (card) => _CardType1(
+              elevation: card['elevation'],
+              label: card['label'],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final double elevation;
+  final String label;
+
+  const _CardType1({super.key, required this.elevation, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: const Icon(Icons.more_vert_outlined),
+                  onPressed: () {},
+                )),
+            Align(alignment: Alignment.bottomLeft, child: Text(label))
+          ],
+        ),
       ),
     );
   }
