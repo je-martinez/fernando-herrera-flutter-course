7d90bebeab2a06d5ec9a3b14731ff6b3a04af0f5
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-06-11 17:34:51 -0600

    chore: wip datasources

--- cinemapedia_app/lib/domain/repositories/movies_repository.dart
+++ cinemapedia_app/lib/domain/repositories/movies_repository.dart
@@ -1,5 +1,5 @@
 import 'package:cinemapedia_app/domain/entities/movie.dart';
 
-abstract class MovieRepository {
-  Future<Movie> getNowPlaying({int page = 1});
+abstract class MoviesRepository {
+  Future<List<Movie>> getNowPlaying({int page = 1});
 }
---
+++ cinemapedia_app/lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia_app/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:cinemapedia_app/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl implements MoviesRepository {
+  final MoviesDatasource moviesDatasource;
+
+  MovieRepositoryImpl({required this.moviesDatasource});
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    return moviesDatasource.getNowPlaying(page: page);
+  }
+}
