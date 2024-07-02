d392a13bbfae7f635391c917a5554d66b2d2db94
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-07-02 15:13:00 -0600

    chore: implementation of datasource

--- cinemapedia_app/lib/infrastructure/datasources/isar_datasource.dart
+++ cinemapedia_app/lib/infrastructure/datasources/isar_datasource.dart
@@ -1,22 +1,49 @@
 import 'package:cinemapedia_app/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia_app/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
-  @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    if (Isar.instanceNames.isEmpty) {
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open([MovieSchema],
+          directory: dir.path, inspector: true);
+    }
+    return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+    final movie = await isar.movies.filter().idEqualTo(movieId).findFirst();
+    return movie != null;
   }
 
   @override
-  Future<void> toogleFavorite(Movie movie) {
-    // TODO: implement toogleFavorite
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) async {
+    final isar = await db;
+    final movies =
+        await isar.movies.where().offset(offset).limit(limit).findAll();
+    return movies;
+  }
+
+  @override
+  Future<void> toogleFavorite(Movie movie) async {
+    final isar = await db;
+    await isar.writeTxn(() async {
+      final movieIsFavorite = await isMovieFavorite(movie.id);
+      if (movieIsFavorite) {
+        await isar.movies.delete(movie.id);
+      } else {
+        await isar.movies.put(movie);
+      }
+    });
   }
 }
