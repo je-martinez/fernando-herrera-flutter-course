284e11fa398064ab0582c57f9146268dd1963117
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-07-04 13:49:50 -0600

    chore: screens needed

--- forms_app/lib/config/router/app_router.dart
+++ forms_app/lib/config/router/app_router.dart
@@ -4,5 +4,6 @@ import 'package:go_router/go_router.dart';
 final appRouter = GoRouter(routes: [
   GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
   GoRoute(
-      path: '/cubits', builder: (context, state) => const CubitCounterScreen())
+      path: '/cubits', builder: (context, state) => const CubitCounterScreen()),
+  GoRoute(path: '/bloc', builder: (context, state) => const BlocCounterScreen())
 ]);
--- forms_app/lib/presentation/screens/bloc_counter_screen.dart
+++ forms_app/lib/presentation/screens/bloc_counter_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CubitCounterScreen extends StatelessWidget {
-  const CubitCounterScreen({super.key});
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
