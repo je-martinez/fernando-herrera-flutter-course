0368fd798e5826ba870d7f7256f7f89bda730c3a
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-07-10 16:53:55 -0600

    chore: creating app

---
+++ push_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:push_app/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
