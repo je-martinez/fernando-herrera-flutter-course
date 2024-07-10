def87810049938746c701a118ed8df1acdb66857
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-07-10 14:04:45 -0600

    chore: traditional form

--- forms_app/lib/presentation/screens/register_screen.dart
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -42,35 +42,87 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm({super.key});
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
-          const CustomTextFormField(
+          CustomTextFormField(
             label: "Username",
+            onChanged: (value) {
+              username = value;
+            },
+            validator: (value) {
+              if (value == null) return 'El campo es obligatorio';
+              if (value.isEmpty) return 'El campo es obligatorio';
+              if (value.trim().isEmpty) return 'El campo es obligatorio';
+              if (value.length < 6) {
+                return 'El campo debe tener al menos 3 caracteres';
+              }
+              return null;
+            },
           ),
           const SizedBox(
             height: 10,
           ),
-          const CustomTextFormField(
+          CustomTextFormField(
             label: "Email",
+            onChanged: (value) {
+              email = value;
+            },
+            validator: (value) {
+              if (value == null) return 'El campo es obligatorio';
+              if (value.isEmpty) return 'El campo es obligatorio';
+              if (value.trim().isEmpty) return 'El campo es obligatorio';
+              final isEmail =
+                  RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value);
+              if (!isEmail) {
+                return 'El campo debe ser un email';
+              }
+              return null;
+            },
           ),
           const SizedBox(
             height: 10,
           ),
-          const CustomTextFormField(
+          CustomTextFormField(
             label: "Password",
             obscureText: true,
+            onChanged: (value) {
+              password = value;
+            },
+            validator: (value) {
+              if (value == null) return 'El campo es obligatorio';
+              if (value.isEmpty) return 'El campo es obligatorio';
+              if (value.trim().isEmpty) return 'El campo es obligatorio';
+              if (value.length < 6) {
+                return 'El campo debe tener al menos 6 caracteres';
+              }
+              return null;
+            },
           ),
           const SizedBox(
             height: 10,
           ),
           FilledButton.tonalIcon(
-            onPressed: () {},
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+            },
             label: const Text('Guardar'),
             icon: const Icon(Icons.save),
           ),
